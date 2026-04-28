c142ed2fff3f39bee9ebd1c6a3f3efff01bce482
Лещенко Дмитро <dimalesenko82@gmail.com>
2026-04-28 20:54:40 +0200

    add JWT guards, roles guard, current user decorator

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -14,15 +15,18 @@ import { AuthController } from './auth.controller';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        secret: config.get<string>('JWT_SECRET')!,
+        secret: config.get<string>('JWT_SECRET') || 'secret',
         signOptions: {
-          expiresIn: (config.get<string>('JWT_EXPIRES_IN') ?? '1h') as any,
+          expiresIn: (config.get<string>('JWT_EXPIRES_IN') || '1h') as any,
         },
       }),
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    JwtStrategy,
+  ],
   exports: [JwtModule],
 })
 export class AuthModule {}
